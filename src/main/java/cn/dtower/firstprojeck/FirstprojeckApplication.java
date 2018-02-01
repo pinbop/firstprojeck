5a3dd22a8b340f89bd3f8860115053f34f15a790
pinbop <otsm5781@163.com>
2018-02-01 18:00:04 +0800

    Initial commit

---
+++ src/main/java/cn/dtower/firstprojeck/FirstprojeckApplication.java
@@ -0,0 +1,12 @@
+package cn.dtower.firstprojeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstprojeckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojeckApplication.class, args);
+	}
+}
